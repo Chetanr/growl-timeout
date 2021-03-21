efa17007ab24f9b973b1fe8fc40ff98ac702c4a1
chetanrevappa <dgrchetan@gmail.com>
2021-03-21 18:38:01 +1100

    v1

--- src/growl.js
+++ src/growl.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import React from "react";
 
-import './growl.css'
+import "./growl.css";
 
 export const Growl = ({ active, message, onDismissed }) => (
   <div className={`growl${active ? " active" : ""}`}>
     {message}
     <div onClick={onDismissed} className="growl-close" />
   </div>
-)
+);
 
 export function useGrowl() {
-    // state of the growl
-    const [growlActive, setGrowlActive] = React.useState(false)
+  // state of the growl
+  const [growlActive, setGrowlActive] = React.useState(false);
 
-    return [
-        // the first arg is the state
-        growlActive, 
+  setTimeout(function () {
+    setGrowlActive(false);
+  }, 3000);
 
-        // the second arg is a fn that allows you to safely set its state
-        (active) => {
-            setGrowlActive(active)
-        },
-    ]
+  return [
+    // the first arg is the state
+    growlActive,
+
+    // the second arg is a fn that allows you to safely set its state
+    (active) => {
+      setGrowlActive(active);
+    },
+  ];
 }
